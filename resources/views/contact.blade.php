528dbee6c8cc982e449e0d3ffcfe72709b9e66f4
Pedro Nunes <pedro.nunesmoura@outlook.com>
2023-04-22 20:02:55 -0300

    Finalização do projeto hdcevents do Canal Hora de Codar

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Página de contato teste</h1>
+<a href="/">Voltar para a página</a>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+    <!--Fonte do Google -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto " rel="stylesheet">
+
+    <!--CSS Bootstap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+
+    <!-- CSS da aplicação -->
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="/js/scripts.js"></script>
+</head>
+<body class="antialiased">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-ligth">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar brand">
+                    <img src="/img/up-logo.jpeg" alt="HDC Events">
+                </a>
+                <ul class="navbar nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/events/create" class="nav-link">Criar Eventos</a>
+                    </li>
+                    @auth
+                    <li class="nav-item">
+                        <a href="/dashboard" class="nav-link">Meus Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <a href="/logout" class="nav-link"
+                                              onclick="event.preventDefault();
+                                              this.closest('form').submit();">
+                                Sair
+                            </a>
+                        </form>
+                    </li>
+                    @endauth
+
+                    @guest
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/register" class="nav-link">Cadastrar</a>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                    <p class="msg">{{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p>HDC Events &copy; 2022</p>
+    </footer>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+</body>
+</html>
